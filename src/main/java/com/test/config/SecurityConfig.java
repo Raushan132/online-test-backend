bb5905fafc62958e427ebce8cdaee5cffb73fc1e
Raushan132 <raushankumar132abc@gmail.com>
2025-08-30 14:44:08 +0530

    add spring security with jwt

---
+++ src/main/java/com/test/config/SecurityConfig.java
@@ -0,0 +1,49 @@
+package com.test.config;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+public class SecurityConfig {
+
+    private final JwtAuthFilter jwtAuthFilter;
+    private final CustomUserDetailsService userDetailsService;
+
+    public SecurityConfig(JwtAuthFilter jwtAuthFilter, CustomUserDetailsService userDetailsService) {
+        this.jwtAuthFilter = jwtAuthFilter;
+        this.userDetailsService = userDetailsService;
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http.csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                		  .requestMatchers("/auth/**").permitAll()   // login & register free
+                          .requestMatchers("/admin/**").hasRole("ADMIN") // only ADMIN
+                          .requestMatchers("/user/**").hasAnyRole("USER", "ADMIN") // USER or ADMIN
+                          .requestMatchers("/swagger-ui/**").permitAll()
+                          .requestMatchers("/**").permitAll()
+                          .anyRequest().authenticated()
+                )
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
+                .build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
+    }
+}
+
--- src/main/java/com/test/repository/UserRepository.java
+++ src/main/java/com/test/repository/UserRepository.java
@@ -1,9 +1,13 @@
 package com.test.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.test.model.UserEntity;
 
 public interface UserRepository extends JpaRepository<UserEntity,Integer>{
+
+	Optional<UserEntity> findByEmail(String email);
 
 }
